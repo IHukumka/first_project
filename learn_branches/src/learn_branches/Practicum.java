9aa7602a16a2ef144c0c9eac1cbbc1cd6c65c7f1
IHukumka <n.a.kiryanov@yandex.ru>
2023-01-24 15:33:45 +0300

    feat: добавить класс Person

--- learn_branches/src/learn_branches/Practicum.java
+++ learn_branches/src/learn_branches/Practicum.java
@@ -1,5 +1,17 @@
 package learn_branches;
 
 public class Practicum {
+    public static void main(String[] args) {
+        Object anyObject = new Person("Михаил");
+        System.out.println(anyObject);
 
+    }
 }
+
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+} 

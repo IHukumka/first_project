1798b3220cd8f8606b8d1ba09afed2c851124ccc
IHukumka <n.a.kiryanov@yandex.ru>
2023-01-24 15:37:51 +0300

    feat: добавить поля в Person

--- learn_branches/src/learn_branches/Practicum.java
+++ learn_branches/src/learn_branches/Practicum.java
@@ -2,16 +2,19 @@ package learn_branches;
 
 public class Practicum {
     public static void main(String[] args) {
-        Object anyObject = new Person("Михаил");
+        Object anyObject = new Person("Иван", "Иванов", 32);
         System.out.println(anyObject);
-
     }
 }
 
 class Person {
-    private String name;
+    private String firstName;
+    private String secondName;
+    private int age;
 
-    public Person(String name) {
-        this.name = name;
+    public Person(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
     }
 } 
